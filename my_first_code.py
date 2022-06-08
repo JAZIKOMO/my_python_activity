ea802f842e69dec1c097624bbc83a0c32c0438d0
JAZIKOMO <103224900+JAZIKOMO@users.noreply.github.com>
2022-06-08 16:42:30 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+print("my name is Ziko")
+print("the date is 08/06/2022")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+
+# Remember Python does not use a comma to separate thousands
+# Create variables
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Add all the sales together 
+total_sales = a + b + c + d + e
+
+# View the output
+print(total_sales)
+
+
+# In[3]:
+
+
+# Create a new variable
+# Calculate the average sales by dividing the total number of sales by 5
+avg_sales = total_sales / 5
+
+# View the output
+print(avg_sales)
+
+
+# In[4]:
+
+
+# create a new variable
+# Calculate the price of property C as sales - commission
+property_price_c = (67000 - 6500)
+
+# View the output
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
